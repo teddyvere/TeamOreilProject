d5fe42e08b50dd511316d1fbcf193ee28d8e8a28
igordon215 <igordon215@gmail.com>
2024-06-28 17:11:18 -0400

    made a change

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -12,6 +12,6 @@ public class MainApplication {
 
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+        //Console.println("The user input %s as a d", d);
     }
 }
