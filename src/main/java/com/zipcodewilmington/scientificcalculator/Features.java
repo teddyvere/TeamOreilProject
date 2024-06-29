730457c4a7d50cd9253a77718fe94b063b3287ae
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-06-28 20:57:18 -0400

    the blueprint

--- src/main/java/com/zipcodewilmington/scientificcalculator/Features.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Features.java
@@ -1,7 +1,38 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class Features {
-    public void setOperator(String operator) {this.operator = operator;}
+    String operator;
+    int answer;
+    int value1=0;
+    int value2=0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void setAnswer(int answer) {
+        this.answer = answer; }
+    public int getValue1() {
+        return value1;}
+    public void setValue1(int value1) {
+        this.value1 = value1; }
+    public void setValue2(int value2) {
+        this.value2 = value2;}
+    public int getValue2() {
+        return value2;}
+    public String getOperator() {
+        return operator;}
+    public void setOperator(String operator) {
+        this.operator = operator;}
     public Features() {
 
     }
@@ -17,5 +48,7 @@ public class Features {
 
         }
         return answer;
-    }
-}
+
+
+
+
